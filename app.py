5facd30a92a95a9b280886089329f9dc82d682e5
a2krocks <aakapurv@gmail.com>
2022-07-04 23:51:39 +0530

    Made it directly shortcut ability

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, redirect
 from flask_session import Session
 from sqlalchemy import create_engine
 from sqlalchemy.exc import NoSuchModuleError
@@ -25,27 +25,35 @@ except NoSuchModuleError:
 db = scoped_session(sessionmaker(bind=engine))
 
 
-@app.route('/<string:code>')
-def get_link(code: str):  # put application's code here
-    if request.method == "POST":
-        return "Invalid Request"
-
+def get_link(code: str) -> dict:
     res_data = db.execute("SELECT * FROM surl WHERE code= :code", {"code": code}).fetchone()
-
     if res_data is None:
         data = {
             "status": 400,
             "msg": "link not found"
         }
-        return jsonify(data), 400
-
+        return data
     data = {
         "status": 200,
         "code": res_data[0],
         "link": res_data[1]
     }
+    return data
 
-    return jsonify(data), 200
+# Api to get link
+@app.route('/<string:code>')
+def index(code: str):  # put application's code here
+    if request.method == "POST":
+        return "Invalid Request", 400
+
+    data = get_link(code)
+    return jsonify(data), data["status"]
+
+
+# Shortcut to redirect directly to link
+@app.route("/r/<string:code>")
+def redirect_to_link(code: str):
+    return redirect(get_link(code)["link"])
 
 
 if __name__ == '__main__':
